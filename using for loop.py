6961c810f22c32def7792839bc43deddf6490696
Krutik Hirudkar <krutikhirudkar721@gmail.com>
2026-07-31 20:30:06 +0530

    Update using for loop.py

--- using for loop.py
+++ using for loop.py
@@ -1,6 +1,6 @@
 #Using for loop Hello Students
 
-n1 = int(input("Enter first number: "))
+n1 = int(input("Enter first number: "));
 n2 = int(input("Enter second number: "))
 n3 = int(input("Enter third number: "))
 
