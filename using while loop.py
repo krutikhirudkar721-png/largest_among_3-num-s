d6751fddb26cd3452096aedf97956ba5df0d2801
Krutik Hirudkar <krutikhirudkar721@gmail.com>
2026-07-31 20:33:47 +0530

    Update using while loop.py

--- using while loop.py
+++ using while loop.py
@@ -3,7 +3,7 @@
 n1 = int(input("Enter first number: "))
 n2 = int(input("Enter second number: "))
 n3 = int(input("Enter third number: "))
-numbers = [n1, n2, n3]
+numbers = [n1, n2., n3]
 
 # Variables for while loop
 
