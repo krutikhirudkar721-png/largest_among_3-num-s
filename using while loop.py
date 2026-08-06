8eb82d5c34f7c838b866c5774756c6c50f691709
Krutik Hirudkar <krutikhirudkar721@gmail.com>
2026-08-06 19:24:42 +0530

    Update using while loop.py

--- using while loop.py
+++ using while loop.py
@@ -14,6 +14,7 @@ index = 0
 while index < len(numbers):
     if numbers[index] > largest:
         largest = numbers[index]
-    index += 1  # Move to next number
+    index += 1  
+    # Move to next number
 
 print(f"The largest number is: {largest}")
